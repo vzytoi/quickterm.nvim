da14e52a591add4ea51ded6a89d0f90840837ff2
vzytoi <65293868+vzytoi@users.noreply.github.com>
2022-12-04 13:33:24 +0100

    feat: use <esc> to enter normal mode

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -1,16 +1,28 @@
+local function keymaps()
+
+    -- quickly leave the terminal
+    vim.keymap.set({ "t", "n" }, "<c-d>", function()
+        vim.api.nvim_command("q")
+    end, { buffer = 0 })
+
+
+    -- use <esc> to enter normal mode
+    vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { buffer = 0 })
+
+end
+
+local function opts()
+    vim.wo.nu = false
+    vim.wo.rnu = false
+end
+
 vim.api.nvim_create_autocmd("TermOpen", {
     callback = function()
         if vim.bo.filetype == "quickterm" then
             vim.cmd("startinsert")
 
-            vim.wo.nu = false
-            vim.wo.rnu = false
-
-            vim.keymap.set("t", "<c-d>", function()
-                vim.api.nvim_command("q")
-            end, {
-                buffer = 0
-            })
+            opts()
+            keymaps()
         end
     end
 })
